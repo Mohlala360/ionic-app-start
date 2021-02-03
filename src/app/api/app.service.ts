9301a3d312a36285874ab6b709ef1929e55649b0
Stanley Mohlala <stanzo360@outlook.com>
2021-02-03 08:28:04 +0200

    Post functions

--- src/app/api/app.service.ts
+++ src/app/api/app.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  private apiUrl = environment.api;
+  constructor(private http: HttpClient) { }
+
+
+  getCars(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl + 'carBooking/cars');
+  }
+
+  addCarBooking(tempCarBooking: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl + 'carBooking', tempCarBooking);
+  }
 
   getCarBookings(carBookings): void {
     carBookings.push(
       {
         carBookingId: 2,
         referenceNo: "CB2",
-        user: { name: "Stanely", surname: "Mohlala", email: "stanleymohlala83@gmail.com" ,cell:"078 289 6472"},
+        user: { name: "Stanely", surname: "Mohlala", email: "stanleymohlala83@gmail.com", cell: "078 289 6472" },
         car: { carId: 1, registrationNumber: "Stanley GP" },
         bookingFrom: '2020-01-02 09:00',
         bookingTo: '2021-01-05 20:00',
@@ -21,7 +34,7 @@ export class AppService {
       {
         carBookingId: 1,
         referenceNo: "CB1",
-        user: { name: "Judith", surname: "Matlala", email: "judithmatlala96@gmail.com" ,cell:"076 767 5812 "},
+        user: { name: "Judith", surname: "Matlala", email: "judithmatlala96@gmail.com", cell: "076 767 5812 " },
         car: { carId: 2, registrationNumber: "Judith GP" },
         bookingFrom: '2020-12-31 14:20',
         bookingTo: '2021-01-02 20:00',
--- src/app/projects/pool-vehicle/pool-vehicle.page.ts
+++ src/app/projects/pool-vehicle/pool-vehicle.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AppService } from 'src/app/api/app.service';
 
 @Component({
@@ -15,10 +15,10 @@ export class PoolVehiclePage implements OnInit {
   booking: any;
   editMode: any;
   title: string;
+  car: any;
 
   constructor(
-    private activatedRoute: ActivatedRoute, private appService: AppService) {
-
+    private activatedRoute: ActivatedRoute, private appService: AppService, private router: Router) {
   }
 
   ngOnInit() {
@@ -35,36 +35,45 @@ export class PoolVehiclePage implements OnInit {
         this.booking = selectedBooking;
         console.log(this.booking);
       } else {
-        this.title = `Add car booking `;       
+        this.title = `Add car booking `;
         this.booking = {
+          carBookingId: 0,
           bookingReason: "",
           user: {
             name: "",
             email: "",
-            cell: ""
+            cell: "",
+            userId: 0
           },
           car: {
+            carId: 0,
             registrationNumber: "",
-          }
+          },
+          bookingFrom: "",
+          bookingTo: ""
         };
       }
     });
-    
+
     this.steps = {
       prev: false,
       next: true
     };
     this.getCars();
-
   }
 
-  nextStep() {
+  setData() {
     var nameValue = (<HTMLInputElement>document.getElementById("txtName")).value;
     var cellValue = (<HTMLInputElement>document.getElementById("txtCell")).value;
     var emailValue = (<HTMLInputElement>document.getElementById("txtEmail")).value;
     var bookingReasonValue = (<HTMLInputElement>document.getElementById("txtbookingReason")).value;
+    var bookingFromDateValue = (<HTMLInputElement>document.getElementById("txtbookingFromDate")).value + ' ' +
+      (<HTMLInputElement>document.getElementById("txtbookingFromTime")).value;
+    var bookingToDateValue = (<HTMLInputElement>document.getElementById("txtbookingToDate")).value + ' ' +
+      (<HTMLInputElement>document.getElementById("txtbookingToTime")).value;
 
     this.user = {
+      userId: 0,
       name: nameValue,
       cell: cellValue,
       email: emailValue
@@ -73,8 +82,14 @@ export class PoolVehiclePage implements OnInit {
     console.log(this.user);
 
     this.booking = {
+      carBookingId: 0,
       bookingReason: bookingReasonValue,
+      userId: 0,
       user: this.user,
+      bookingTo: bookingToDateValue,
+      bookingFrom: bookingFromDateValue,
+      carId: this.car.carId,
+      car: this.car
     };
 
     this.steps = {
@@ -85,19 +100,27 @@ export class PoolVehiclePage implements OnInit {
 
   getCars() {
     this.cars = [];
-    this.cars.push({ carId: "1", registrationNumber: "Stanley GP" });
-    this.cars.push({ carId: "2", registrationNumber: "Judith GP" });
-    this.cars.push({ carId: "3", registrationNumber: "Thuto GP" });
+    this.appService.getCars().subscribe((returnedCars: any[]) => {
+      console.log('cars = ', returnedCars);
+      this.cars = returnedCars;
+    });
   }
 
   selectedCar(car): void {
-    const k = car;
+    let id = car.target.value;
+    this.car = this.cars.filter(b => b.carId == id)[0];
+  }
+
+  submit(): void {
+    this.setData();
+    console.log('booking = ', this.booking);
+    this.appService.addCarBooking(this.booking).subscribe((booking: any) => {
+      if (booking.referenceNo !== undefined) {
+        this.router.navigate(['/projects']);
+      } else {
+        //failing methods here
+      }
+    });
   }
 
 }
-
-// export class User {
-//   name: "";
-//   email: "";
-//   cell: "";
-// }
